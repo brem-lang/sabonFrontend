c9148f798c5c353606f905b0ff6252fb6789ab68
Brem0317 <patrickbarcelo17@gmail.com>
2024-12-05 23:59:37 +0800

    setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build",
+    chunkSizeWarningLimit: 1000, // Increase chunk size limit to 1 MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.split("node_modules/")[1].split("/")[0]; // Split major node_modules into separate chunks
+          }
+        },
+      },
+    },
+  },
+});
